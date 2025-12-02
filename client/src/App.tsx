9fa0c9a11420e885f6a68128d06a6a6e1785fe74
lin2398en3 <lin2398en3@users.noreply.github.com>
2025-12-02 14:48:39 -0500

    Fix routing for GitHub Pages - switch to HashRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import OwnerPortalPage from "./pages/OwnerPortal";
 import "./App.css";
 
 function App() {
+  // Use HashRouter for GitHub Pages compatibility
+  // HashRouter uses hash-based routing (#/) which works with static hosting
+  // URLs will be: /#/ and /#/owners
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/owners" element={<OwnerPortalPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
